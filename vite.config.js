6e6874f4f195fda4c00614bf8b6037ba24f8a1f4
karthick Sri Ram <karthicksriram10@gmail.com>
2025-09-24 18:56:17 +0530

    Remove Firebase default public folder

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/karthick_portfolio/", // your repo name here
+  base: "/", // your repo name here
   plugins: [react()],
 });
